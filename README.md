7fe08d2c6ba57ab717881a28f4971c2c7bb7e86e
Lars <lkbr@live.dk>
2020-02-04 16:19:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 02450Projects
+# 02450Projects
+# 02450Projects
---
+++ test.py
@@ -0,0 +1,7 @@
+import sys
+
+sys.path.insert(1, "02450Toolbox_Python/Tools/toolbox_02450")
+
+import statistics
+
+statistics.ciao()
