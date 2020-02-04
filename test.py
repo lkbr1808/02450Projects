ad91fab7edf39c766255270468e4dfd908f47b1a
Durita <s173807>
2020-02-04 17:02:04 +0100

    asd

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import sys
+import numpy as np
 
-sys.path.insert(1, "02450Toolbox_Python/Tools/toolbox_02450")
+sys.path.insert(1, "Tools/toolbox_02450")
 
 import statistics
 
 statistics.ciao()
+
+a = np.arange(1,20,4)
+print(a)
