d9eac315eeae71da8ce3ad14aed48b04055479dc
Gustav Surface <s183934>
2020-02-29 12:47:51 +0100

    data visualization

--- oz_BoksplotSeasonal.py
+++ oz_BoksplotSeasonal.py
@@ -1,14 +1,11 @@
-# requires data from exercise 4.1.1
+# Import data from other script, standardized data matrix is named Y 
 from oz_Import import *
 
 plt.figure()
 for c in range(1,C2+1):
     plt.subplot(1,C2,c)
-    class_mask = (y==c) # binary mask to extract elements of class c
-    # or: class_mask = nonzero(y==c)[0].tolist()[0] # indices of class c
-    
+    class_mask = (y==c)
     plt.boxplot(Y[class_mask,:])
-    #title('Class: {0}'.format(classNames[c]))
     plt.title('Class: '+classNames2[c-1])
     plt.xticks(range(1,M+1),attributeNames2, rotation=-45)
 
---
+++ oz_DataMatrix.py
@@ -0,0 +1,14 @@
+from scipy.stats import zscore
+
+# Import data from other script, standardized data matrix is named Y 
+from oz_Import import *
+
+plt.figure(figsize=(12,6))
+plt.imshow(Y, interpolation='none', aspect=(10./N), cmap=plt.cm.gray);
+plt.xticks(range(10), attributeNames2,rotation=-45)
+plt.xlabel('Attributes')
+plt.ylabel('Data objects')
+plt.title("La Ozone '76 data matrix")
+plt.colorbar()
+
+plt.show()
