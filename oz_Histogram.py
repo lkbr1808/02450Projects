ea101a790e1129a85c92e7f84c0bbc7fc221cf6c
Gustav Surface <s183934>
2020-03-02 15:31:02 +0100

    fixed histogram

--- oz_Histogram.py
+++ oz_Histogram.py
@@ -5,7 +5,7 @@ from ImportData import *
 plt.figure(figsize=(18, 10))
 for m in range(M-4):
     plt.subplot(1, M-4, m+1)
-    plt.hist(Y[:, m])
+    plt.hist(X[:, m])
     plt.title(attributeNames2[m])
     plt.ylim(top=120, bottom=0)
     if m != 0:
