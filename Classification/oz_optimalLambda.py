05b51089e3ad7ddc27e27543a7caf9b4b05653e1
Gustav Surface <s183934>
2020-04-16 16:59:24 +0200

    Multi-level cross-validation on multinomial regression to get optimal lambda

--- Classification/oz_optimalLambda.py
+++ Classification/oz_optimalLambda.py
@@ -1,134 +1,71 @@
+# exercise 8.1.2
 
-import sklearn.linear_model as lm
-from sklearn import model_selection
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
 import sys
 sys.path.insert(1, "Tools/toolbox_02450")
-from __init__ import rlr_validate
+from __init__ import rocplot, confmatplot
 
 from importData import *
 
-# mat_data = loadmat('./Data/body.mat')
-# X = mat_data['X']
-# y = mat_data['y'].squeeze()
-# attributeNames = [name[0] for name in mat_data['attributeNames'][0]]
-# N, M = X.shape
+font_size = 15
+plt.rcParams.update({'font.size': font_size})
 
-# Add offset attribute
-X = np.concatenate((np.ones((X.shape[0],1)),X),1)
-attributeNames = [u'Offset']+attributeNames
-M = M+1
+# Load Matlab data file and extract variables of interest
 
-## Crossvalidation
+
 # Create crossvalidation partition for evaluation
-K = 5
-CV = model_selection.KFold(K, shuffle=True)
-#CV = model_selection.KFold(K, shuffle=False)
+# using stratification and 95 pct. split between training and test 
+K = 20
+MR_X_train, MR_X_test, MR_y_train, MR_y_test = train_test_split(X_train, y_train, test_size=.95, stratify=y)
+# Try to change the test_size to e.g. 50 % and 99 % - how does that change the 
+# effect of regularization? How does differetn runs of  test_size=.99 compare 
+# to eachother?
 
-# Values of lambda
-lambdas = np.power(10.,range(-5,9))
+# Standardize the training and set set based on training set mean and std
 
-# Initialize variables
-#T = len(lambdas)
-Error_train = np.empty((K,1))
-Error_test = np.empty((K,1))
-Error_train_rlr = np.empty((K,1))
-Error_test_rlr = np.empty((K,1))
-Error_train_nofeatures = np.empty((K,1))
-Error_test_nofeatures = np.empty((K,1))
-w_rlr = np.empty((M,K))
-mu = np.empty((K, M-1))
-sigma = np.empty((K, M-1))
-w_noreg = np.empty((M,K))
+# Fit regularized logistic regression model to training data to predict 
+# the type of wine
+lambda_interval = np.logspace(-8, 2, 50)
+train_error_rate = np.zeros(len(lambda_interval))
+test_error_rate = np.zeros(len(lambda_interval))
+coefficient_norm = np.zeros(len(lambda_interval))
 
-k=0
-for train_index, test_index in CV.split(X,y):
+for k in range(0, len(lambda_interval)):
+    mdl = LogisticRegression(penalty='l2', C=1/lambda_interval[k] )
     
-    # extract training and test set for current CV fold
-    X_train = X[train_index]
-    y_train = y[train_index]
-    X_test = X[test_index]
-    y_test = y[test_index]
-    internal_cross_validation = 10    
+    mdl.fit(MR_X_train, MR_y_train)
+
+    y_train_est = mdl.predict(MR_X_train).T
+    y_test_est = mdl.predict(MR_X_test).T
     
-    opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(X_train, y_train, lambdas, internal_cross_validation)
+    train_error_rate[k] = np.sum(y_train_est != MR_y_train) / len(MR_y_train)
+    test_error_rate[k] = np.sum(y_test_est != MR_y_test) / len(MR_y_test)
 
-    # Standardize outer fold based on training set, and save the mean and standard
-    # deviations since they're part of the model (they would be needed for
-    # making new predictions) - for brevity we won't always store these in the scripts
-    mu[k, :] = np.mean(X_train[:, 1:], 0)
-    sigma[k, :] = np.std(X_train[:, 1:], 0)
-    
-    X_train[:, 1:] = (X_train[:, 1:] - mu[k, :] ) / sigma[k, :] 
-    X_test[:, 1:] = (X_test[:, 1:] - mu[k, :] ) / sigma[k, :] 
-    
-    Xty = X_train.T @ y_train
-    XtX = X_train.T @ X_train
-    
-    # Compute mean squared error without using the input data at all
-    Error_train_nofeatures[k] = np.square(y_train-y_train.mean()).sum(axis=0)/y_train.shape[0]
-    Error_test_nofeatures[k] = np.square(y_test-y_test.mean()).sum(axis=0)/y_test.shape[0]
+    w_est = mdl.coef_[0] 
+    coefficient_norm[k] = np.sqrt(np.sum(w_est**2))
 
-    # Estimate weights for the optimal value of lambda, on entire training set
-    lambdaI = opt_lambda * np.eye(M)
-    lambdaI[0,0] = 0 # Do no regularize the bias term
-    w_rlr[:,k] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
-    # Compute mean squared error with regularization with optimal lambda
-    Error_train_rlr[k] = np.square(y_train-X_train @ w_rlr[:,k]).sum(axis=0)/y_train.shape[0]
-    Error_test_rlr[k] = np.square(y_test-X_test @ w_rlr[:,k]).sum(axis=0)/y_test.shape[0]
+min_error = np.min(test_error_rate)
+opt_lambda_idx = np.argmin(test_error_rate)
+opt_lambda = lambda_interval[opt_lambda_idx]
 
-    # Estimate weights for unregularized linear regression, on entire training set
-    w_noreg[:,k] = np.linalg.solve(XtX,Xty).squeeze()
-    # Compute mean squared error without regularization
-    Error_train[k] = np.square(y_train-X_train @ w_noreg[:,k]).sum(axis=0)/y_train.shape[0]
-    Error_test[k] = np.square(y_test-X_test @ w_noreg[:,k]).sum(axis=0)/y_test.shape[0]
-    # OR ALTERNATIVELY: you can use sklearn.linear_model module for linear regression:
-    #m = lm.LinearRegression().fit(X_train, y_train)
-    #Error_train[k] = np.square(y_train-m.predict(X_train)).sum()/y_train.shape[0]
-    #Error_test[k] = np.square(y_test-m.predict(X_test)).sum()/y_test.shape[0]
-
-    # Display the results for the last cross-validation fold
-    if k == K-1:
-        plt.figure(k, figsize=(12,8))
-        plt.subplot(1,2,1)
-        plt.semilogx(lambdas,mean_w_vs_lambda.T[:,1:],'.-') # Don't plot the bias term
-        plt.xlabel('Regularization factor')
-        plt.ylabel('Mean Coefficient Values')
-        plt.grid()
-        # You can choose to display the legend, but it's omitted for a cleaner 
-        # plot, since there are many attributes
-        #legend(attributeNames[1:], loc='best')
-        
-        plt.subplot(1,2,2)
-        plt.title('Optimal lambda: 1e{0}'.format(np.log10(opt_lambda)))
-        plt.loglog(lambdas,train_err_vs_lambda.T,'b.-',lambdas,test_err_vs_lambda.T,'r.-')
-        plt.xlabel('Regularization factor')
-        plt.ylabel('Squared error (crossvalidation)')
-        plt.legend(['Train error','Validation error'])
-        plt.grid()
-    
-    # To inspect the used indices, use these print statements
-    #print('Cross validation fold {0}/{1}:'.format(k+1,K))
-    #print('Train indices: {0}'.format(train_index))
-    #print('Test indices: {0}\n'.format(test_index))
-
-    k+=1
+plt.figure(figsize=(8,8))
+#plt.plot(np.log10(lambda_interval), train_error_rate*100)
+#plt.plot(np.log10(lambda_interval), test_error_rate*100)
+#plt.plot(np.log10(opt_lambda), min_error*100, 'o')
+plt.semilogx(lambda_interval, train_error_rate*100)
+plt.semilogx(lambda_interval, test_error_rate*100)
+plt.semilogx(opt_lambda, min_error*100, 'o')
+plt.text(1e-8, 3, "Minimum test error: " + str(np.round(min_error*100,2)) + ' % at 1e' + str(np.round(np.log10(opt_lambda),2)))
+plt.xlabel('Regularization strength, $\log_{10}(\lambda)$')
+plt.ylabel('Error rate (%)')
+plt.title('Classification error')
+plt.legend(['Training error','Test error','Test minimum'],loc='upper right')
+plt.ylim([0, 4])
+plt.grid()
+plt.show()    
 
 
-# Display results
-print('Linear regression without feature selection:')
-print('- Training error: {0}'.format(Error_train.mean()))
-print('- Test error:     {0}'.format(Error_test.mean()))
-print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train.sum())/Error_train_nofeatures.sum()))
-print('- R^2 test:     {0}\n'.format((Error_test_nofeatures.sum()-Error_test.sum())/Error_test_nofeatures.sum()))
-print('Regularized linear regression:')
-print('- Training error: {0}'.format(Error_train_rlr.mean()))
-print('- Test error:     {0}'.format(Error_test_rlr.mean()))
-print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train_rlr.sum())/Error_train_nofeatures.sum()))
-print('- R^2 test:     {0}\n'.format((Error_test_nofeatures.sum()-Error_test_rlr.sum())/Error_test_nofeatures.sum()))
-
-print('Weights in last fold:')
-for m in range(M):
-    print('{:>15} {:>15}'.format(attributeNames[m], np.round(w_rlr[m,-1],2)))
-
-
-plt.show()
+print('Ran Exercise 9.1.1')
