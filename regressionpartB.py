54a409ea83fddce4830fe35e7ecc925d60ed1ff9
Lars Refsgaard <larsrefsgaard@macMine.local>
2020-04-16 09:32:51 +0200

    cleanup

--- regressionpartB.py
+++ regressionpartB.py
@@ -1,5 +1,5 @@
-from matplotlib.pylab import (figure, semilogx, loglog, xlabel, ylabel, legend, 
-                           title, subplot, show, grid)
+from matplotlib.pylab import (figure, semilogx, loglog, xlabel, ylabel, legend,
+                              title, subplot, show, grid)
 import numpy as np
 from scipy.io import loadmat
 import sklearn.linear_model as lm
@@ -13,86 +13,93 @@ from importData import *
 # Split dataset into features and target vector
 ozone_idx = attributeNames.index('Upland Maximum Ozone')
 
-#Load y and X
-y = X[:,ozone_idx]
-X_cols = list(range(0,ozone_idx)) + list(range(ozone_idx+1,len(attributeNames)))
-X = X[:,X_cols]
+# Load y and X
+y = X[:, ozone_idx]
+X_cols = list(range(0, ozone_idx)) + \
+    list(range(ozone_idx+1, len(attributeNames)))
+X = X[:, X_cols]
 N, M = X.shape
 
 
-
 # Add offset attribute
-X = np.concatenate((np.ones((X.shape[0],1)),X),1)
+X = np.concatenate((np.ones((X.shape[0], 1)), X), 1)
 attributeNames = [u'Offset']+attributeNamesudeny
 M = M+1
 print(X)
 
-## Crossvalidation
+# Crossvalidation
 # Create crossvalidation partition for evaluation
 K = 10
 CV = model_selection.KFold(K, shuffle=True)
 #CV = model_selection.KFold(K, shuffle=False)
 
 # Values of lambda
-lambdas = np.power(10.,range(-2,8))
+lambdas = np.power(10., range(-2, 8))
 
 # Initialize variables
 #T = len(lambdas)
-Error_train = np.empty((K,1))
-Error_test = np.empty((K,1))
-Error_train_rlr = np.empty((K,1))
-Error_test_rlr = np.empty((K,1))
-Error_train_nofeatures = np.empty((K,1))
-Error_test_nofeatures = np.empty((K,1))
-w_rlr = np.empty((M,K))
+Error_train = np.empty((K, 1))
+Error_test = np.empty((K, 1))
+Error_train_rlr = np.empty((K, 1))
+Error_test_rlr = np.empty((K, 1))
+Error_train_nofeatures = np.empty((K, 1))
+Error_test_nofeatures = np.empty((K, 1))
+w_rlr = np.empty((M, K))
 mu = np.empty((K, M-1))
 sigma = np.empty((K, M-1))
-w_noreg = np.empty((M,K))
+w_noreg = np.empty((M, K))
 
 
-k=0
-for train_index, test_index in CV.split(X,y):
+k = 0
+for train_index, test_index in CV.split(X, y):
     # extract training and test set for current CV fold
-    print(CV.split(X,y))
+    print(CV.split(X, y))
     X_train = X[train_index]
     y_train = y[train_index]
     X_test = X[test_index]
     y_test = y[test_index]
-    internal_cross_validation = 10    
-    
-    opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(X_train, y_train, lambdas, internal_cross_validation)
+    internal_cross_validation = 10
+
+    opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(
+        X_train, y_train, lambdas, internal_cross_validation)
     print("opt_lambda")
     print(opt_lambda)
-    print(test_err_vs_lambda)   
+    print(test_err_vs_lambda)
     # Standardize outer fold based on training set, and save the mean and standard
     # deviations since they're part of the model (they would be needed for
     # making new predictions) - for brevity we won't always store these in the scripts
     mu[k, :] = np.mean(X_train[:, 1:], 0)
     sigma[k, :] = np.std(X_train[:, 1:], 0)
-    
-    X_train[:, 1:] = (X_train[:, 1:] - mu[k, :] ) / sigma[k, :] 
-    X_test[:, 1:] = (X_test[:, 1:] - mu[k, :] ) / sigma[k, :] 
-    
+
+    X_train[:, 1:] = (X_train[:, 1:] - mu[k, :]) / sigma[k, :]
+    X_test[:, 1:] = (X_test[:, 1:] - mu[k, :]) / sigma[k, :]
+
     Xty = X_train.T @ y_train
     XtX = X_train.T @ X_train
-    
+
     # Compute mean squared error without using the input data at all
-    Error_train_nofeatures[k] = np.square(y_train-y_train.mean()).sum(axis=0)/y_train.shape[0]
-    Error_test_nofeatures[k] = np.square(y_test-y_test.mean()).sum(axis=0)/y_test.shape[0]
+    Error_train_nofeatures[k] = np.square(
+        y_train-y_train.mean()).sum(axis=0)/y_train.shape[0]
+    Error_test_nofeatures[k] = np.square(
+        y_test-y_test.mean()).sum(axis=0)/y_test.shape[0]
 
     # Estimate weights for the optimal value of lambda, on entire training set
     lambdaI = opt_lambda * np.eye(M)
-    lambdaI[0,0] = 0 # Do no regularize the bias term
-    w_rlr[:,k] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
+    lambdaI[0, 0] = 0  # Do no regularize the bias term
+    w_rlr[:, k] = np.linalg.solve(XtX+lambdaI, Xty).squeeze()
     # Compute mean squared error with regularization with optimal lambda
-    Error_train_rlr[k] = np.square(y_train-X_train @ w_rlr[:,k]).sum(axis=0)/y_train.shape[0]
-    Error_test_rlr[k] = np.square(y_test-X_test @ w_rlr[:,k]).sum(axis=0)/y_test.shape[0]
+    Error_train_rlr[k] = np.square(
+        y_train-X_train @ w_rlr[:, k]).sum(axis=0)/y_train.shape[0]
+    Error_test_rlr[k] = np.square(
+        y_test-X_test @ w_rlr[:, k]).sum(axis=0)/y_test.shape[0]
 
     # Estimate weights for unregularized linear regression, on entire training set
-    w_noreg[:,k] = np.linalg.solve(XtX,Xty).squeeze()
+    w_noreg[:, k] = np.linalg.solve(XtX, Xty).squeeze()
     # Compute mean squared error without regularization
-    Error_train[k] = np.square(y_train-X_train @ w_noreg[:,k]).sum(axis=0)/y_train.shape[0]
-    Error_test[k] = np.square(y_test-X_test @ w_noreg[:,k]).sum(axis=0)/y_test.shape[0]
+    Error_train[k] = np.square(
+        y_train-X_train @ w_noreg[:, k]).sum(axis=0)/y_train.shape[0]
+    Error_test[k] = np.square(
+        y_test-X_test @ w_noreg[:, k]).sum(axis=0)/y_test.shape[0]
     # OR ALTERNATIVELY: you can use sklearn.linear_model module for linear regression:
     #m = lm.LinearRegression().fit(X_train, y_train)
     #Error_train[k] = np.square(y_train-m.predict(X_train)).sum()/y_train.shape[0]
@@ -100,32 +107,37 @@ for train_index, test_index in CV.split(X,y):
 
     # Display the results for the last cross-validation fold
     if k == K-1:
-        figure(k, figsize=(12,8))
+        figure(k, figsize=(12, 8))
         title('Optimal lambda: 1e{0}'.format(np.log10(opt_lambda)))
-        loglog(lambdas,train_err_vs_lambda.T,'b.-',lambdas,test_err_vs_lambda.T,'r.-')
+        loglog(lambdas, train_err_vs_lambda.T, 'b.-',
+               lambdas, test_err_vs_lambda.T, 'r.-')
         xlabel('Regularization factor')
         ylabel('Squared error (crossvalidation)')
-        legend(['Train error','Validation error'])
+        legend(['Train error', 'Validation error'])
         grid()
-    
+
     # To inspect the used indices, use these print statements
     #print('Cross validation fold {0}/{1}:'.format(k+1,K))
     #print('Train indices: {0}'.format(train_index))
     #print('Test indices: {0}\n'.format(test_index))
-    
+
     # Display results
     print('Linear regression without feature selection:')
     print('- Training error: {0}'.format(Error_train.mean()))
     print('- Test error:     {0}'.format(Error_test.mean()))
-    print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train.sum())/Error_train_nofeatures.sum()))
-    print('- R^2 test:     {0}\n'.format((Error_test_nofeatures.sum()-Error_test.sum())/Error_test_nofeatures.sum()))
+    print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum() -
+                                         Error_train.sum())/Error_train_nofeatures.sum()))
+    print('- R^2 test:     {0}\n'.format((Error_test_nofeatures.sum() -
+                                          Error_test.sum())/Error_test_nofeatures.sum()))
     print('Regularized linear regression:')
     print('- Training error: {0}'.format(Error_train_rlr.mean()))
     print('- Test error:     {0}'.format(Error_test_rlr.mean()))
-    print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train_rlr.sum())/Error_train_nofeatures.sum()))
-    print('- R^2 test:     {0}\n'.format((Error_test_nofeatures.sum()-Error_test_rlr.sum())/Error_test_nofeatures.sum()))
+    print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum() -
+                                         Error_train_rlr.sum())/Error_train_nofeatures.sum()))
+    print('- R^2 test:     {0}\n'.format((Error_test_nofeatures.sum() -
+                                          Error_test_rlr.sum())/Error_test_nofeatures.sum()))
 
-    k+=1
+    k += 1
 show()
 
 # Display results
@@ -142,19 +154,9 @@ show()
 
 
 #print('Weights in last fold:')
-#for m in range(M):
+# for m in range(M):
 #    print('{:>15} {:>15}'.format(attributeNames[m], np.round(w_rlr[m,-1],2)))
 #print('Ran Exercise 8.1.1')
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -165,21 +167,21 @@ show()
 
 # Fit ordinary least squares regression model
 #model = lm.LinearRegression()
-#model.fit(X,y)
+# model.fit(X,y)
 
 # Predict ozone content
 #y_est = model.predict(X)
 #residual = y_est-y
-#print(residual)
+# print(residual)
 
 # Display scatter plot
-#figure()
-#subplot(2,1,1)
+# figure()
+# subplot(2,1,1)
 #plot(y, y_est, '.')
 #xlabel('Ozone content (true)'); ylabel('Ozone content (estimated)');
-#subplot(2,1,2)
-#hist(residual,40)
+# subplot(2,1,2)
+# hist(residual,40)
 
-#show()
+# show()
 
 print('Ran Regression')
