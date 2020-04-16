f7804286d87f97a8e84ce7b6220bb1e5b0206150
Gustav Surface <s183934>
2020-04-16 17:23:14 +0200

    Multinomial regression

--- Classification/oz_OptimalLambda.py
+++ Classification/oz_OptimalLambda.py
@@ -13,21 +13,10 @@ from importData import *
 font_size = 15
 plt.rcParams.update({'font.size': font_size})
 
-# Load Matlab data file and extract variables of interest
 
+K = 30
+MR_X_train, MR_X_test, MR_y_train, MR_y_test = train_test_split(Y, y, test_size=(100-100/K)/100, stratify=y)
 
-# Create crossvalidation partition for evaluation
-# using stratification and 95 pct. split between training and test 
-K = 20
-MR_X_train, MR_X_test, MR_y_train, MR_y_test = train_test_split(X_train, y_train, test_size=.95, stratify=y)
-# Try to change the test_size to e.g. 50 % and 99 % - how does that change the 
-# effect of regularization? How does differetn runs of  test_size=.99 compare 
-# to eachother?
-
-# Standardize the training and set set based on training set mean and std
-
-# Fit regularized logistic regression model to training data to predict 
-# the type of wine
 lambda_interval = np.logspace(-8, 2, 50)
 train_error_rate = np.zeros(len(lambda_interval))
 test_error_rate = np.zeros(len(lambda_interval))
@@ -52,9 +41,7 @@ opt_lambda_idx = np.argmin(test_error_rate)
 opt_lambda = lambda_interval[opt_lambda_idx]
 
 plt.figure(figsize=(8,8))
-#plt.plot(np.log10(lambda_interval), train_error_rate*100)
-#plt.plot(np.log10(lambda_interval), test_error_rate*100)
-#plt.plot(np.log10(opt_lambda), min_error*100, 'o')
+
 plt.semilogx(lambda_interval, train_error_rate*100)
 plt.semilogx(lambda_interval, test_error_rate*100)
 plt.semilogx(opt_lambda, min_error*100, 'o')
@@ -66,6 +53,3 @@ plt.legend(['Training error','Test error','Test minimum'],loc='upper right')
 plt.ylim([0, 4])
 plt.grid()
 plt.show()    
-
-
-print('Ran Exercise 9.1.1')
