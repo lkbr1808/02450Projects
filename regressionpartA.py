ddbef942b602aab376dc265265d59c1a0cb779dc
DuritaKvilt <Durita@askodan.fo>
2020-04-16 15:50:56 +0200

    changes

--- regressionpartA.py
+++ regressionpartA.py
@@ -29,118 +29,70 @@ M = M+1
 # Create crossvalidation partition for evaluation
 K = 10
 CV = model_selection.KFold(K, shuffle=True)
-#CV = model_selection.KFold(K, shuffle=False)
 
 # Values of lambda
-lambdas = np.power(10.,range(-2,8))
-
-# Initialize variables
-#T = len(lambdas)
-Error_train = np.empty((K,1))
-Error_test = np.empty((K,1))
-Error_train_rlr = np.empty((K,1))
-Error_test_rlr = np.empty((K,1))
-Error_train_nofeatures = np.empty((K,1))
-Error_test_nofeatures = np.empty((K,1))
-w_rlr = np.empty((M,K))
-mu = np.empty((K, M-1))
-sigma = np.empty((K, M-1))
-w_noreg = np.empty((M,K))
+lambdas = np.power(0.1,range(-6,2))
 
 
-k=0
+CV = model_selection.KFold(K, shuffle=True)
+M = X.shape[1]
+w = np.empty((M,K,len(lambdas)))
+train_error = np.empty((K,len(lambdas)))
+test_error = np.empty((K,len(lambdas)))
+f = 0
+y = y.squeeze()
 for train_index, test_index in CV.split(X,y):
-    # extract training and test set for current CV fold
     X_train = X[train_index]
     y_train = y[train_index]
     X_test = X[test_index]
     y_test = y[test_index]
     
-    opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(X_train, y_train, lambdas)
- 
-    # Standardize outer fold based on training set, and save the mean and standard
-    # deviations since they're part of the model (they would be needed for
-    # making new predictions) - for brevity we won't always store these in the scripts
-    mu[k, :] = np.mean(X_train[:, 1:], 0)
-    sigma[k, :] = np.std(X_train[:, 1:], 0)
+    # Standardize the training and set set based on training set moments
+    mu = np.mean(X_train[:, 1:], 0)
+    sigma = np.std(X_train[:, 1:], 0)
     
-    X_train[:, 1:] = (X_train[:, 1:] - mu[k, :] ) / sigma[k, :] 
-    X_test[:, 1:] = (X_test[:, 1:] - mu[k, :] ) / sigma[k, :] 
+    X_train[:, 1:] = (X_train[:, 1:] - mu) / sigma
+    X_test[:, 1:] = (X_test[:, 1:] - mu) / sigma
     
+    # precompute terms
     Xty = X_train.T @ y_train
     XtX = X_train.T @ X_train
+    for l in range(0,len(lambdas)):
+        # Compute parameters for current value of lambda and current CV fold
+        # note: "linalg.lstsq(a,b)" is substitue for Matlab's left division operator "\"
+        lambdaI = lambdas[l] * np.eye(M)
+        lambdaI[0,0] = 0 # remove bias regularization
+        w[:,f,l] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
+        # Evaluate training and test performance
+        train_error[f,l] = np.power(y_train-X_train @ w[:,f,l].T,2).mean(axis=0)
+        test_error[f,l] = np.power(y_test-X_test @ w[:,f,l].T,2).mean(axis=0)
 
-    # Estimate weights for the optimal value of lambda, on entire training set
-    lambdaI = opt_lambda * np.eye(M)
-    lambdaI[0,0] = 0 # Do no regularize the bias term
-    w_rlr[:,k] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
-    # Compute mean squared error with regularization with optimal lambda
-    Error_train_rlr[k] = np.square(y_train-X_train @ w_rlr[:,k]).sum(axis=0)/y_train.shape[0]
-    Error_test_rlr[k] = np.square(y_test-X_test @ w_rlr[:,k]).sum(axis=0)/y_test.shape[0]
-
-    # OR ALTERNATIVELY: you can use sklearn.linear_model module for linear regression:
-    #m = lm.LinearRegression().fit(X_train, y_train)
-    #Error_train[k] = np.square(y_train-m.predict(X_train)).sum()/y_train.shape[0]
-    #Error_test[k] = np.square(y_test-m.predict(X_test)).sum()/y_test.shape[0]
+    opt_val_err = np.min(np.mean(test_error,axis=0))
+    opt_lambda = lambdas[np.argmin(np.mean(test_error,axis=0))]
+    print(opt_lambda)
+    train_err_vs_lambda = np.mean(train_error,axis=0)
+    test_err_vs_lambda = np.mean(test_error,axis=0)
+    mean_w_vs_lambda = np.squeeze(np.mean(w,axis=1))
 
     # Display the results for the last cross-validation fold
-    if k == K-1:
-        figure(k, figsize=(12,8))
+    if f == K-1:
+        figure(f, figsize=(12,8))
         title('Optimal lambda: 1e{0}'.format(np.log10(opt_lambda)))
         loglog(lambdas,train_err_vs_lambda.T,'b.-',lambdas,test_err_vs_lambda.T,'r.-')
-        xlabel('Regularization factor')
+        xlabel('Regularization factor lambda')
         ylabel('Squared error (crossvalidation)')
         legend(['Train error','Validation error'])
         grid()
-    
-    # To inspect the used indices, use these print statements
-    #print('Cross validation fold {0}/{1}:'.format(k+1,K))
-    #print('Train indices: {0}'.format(train_index))
-    #print('Test indices: {0}\n'.format(test_index))
-    
-    # Display results
-  
-    k+=1
+
+    f=f+1
 show()
+    
 
-print('Weights in last fold:')
+
+print('Weights of folds:')
 for m in range(M):
-    print('{:>15} {:>15}'.format(attributeNames[m], np.round(w_rlr[m,-1],2)))
-print('Ran Exercise 8.1.1')
+    print(attributeNames[m])
+    print(np.round(w[m,-1],2))
+#    print('{:>15} {:>15}'.format(attributeNames[m], np.round(w[m,-1],2)))
 
 
-
-
-
-
-
-
-
-
-
-
-#y = X[:,ozone_idx]
-
-#X_cols = list(range(0,ozone_idx)) + list(range(ozone_idx+1,len(attributeNames)))
-#X = X[:,X_cols]
-
-# Fit ordinary least squares regression model
-#model = lm.LinearRegression()
-#model.fit(X,y)
-
-# Predict ozone content
-#y_est = model.predict(X)
-#residual = y_est-y
-#print(residual)
-
-# Display scatter plot
-#figure()
-#subplot(2,1,1)
-#plot(y, y_est, '.')
-#xlabel('Ozone content (true)'); ylabel('Ozone content (estimated)');
-#subplot(2,1,2)
-#hist(residual,40)
-
-#show()
-
-print('Ran Regression')
